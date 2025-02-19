663bc67cc1f0af87a86f29f193b2c959fe0b37fb
Krushna Kadam <krushna.kadam@alixpartners.com>
2025-02-19 17:06:23 +0530

    expose headers to client

--- app/main.py
+++ app/main.py
@@ -19,7 +19,9 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],  # Expose all response headers to the client
 )
+
 
 
 app.include_router(excel_operations.router)
